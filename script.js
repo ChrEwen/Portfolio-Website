ea9fbdaa526e9be7b0c63750ded8149a7181337d
ChrEwen <142896274+ChrEwen@users.noreply.github.com>
2023-08-22 15:02:17 +0200

    Nav Background Color, Cards Width

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector("header");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  header.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((navLink) =>
   navLink.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    header.classList.remove("active");
   })
 );
